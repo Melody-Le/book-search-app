50590ccd1499e1838eca6f1103d90a1c55e4a049
tanmui1992 <tanmui1992@gmail.com>
2022-12-10 17:04:26 +0800

    feat: success convert image into text

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,43 +2,41 @@ import React, { useEffect, useState } from "react";
 import { Typography, Grid, Box, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Tesseract from "tesseract.js";
+import { createWorker } from "tesseract.js";
 
 function SearchBar() {
   const [isLoading, setIsLoading] = useState(false);
-  const [text, setText] = useState("");
-  const [previewAvatar, setPreviewAvatar] = useState(null);
+  const [textConvert, setTextConvert] = useState("");
+  const [previewImgUrl, setPreviewImgUrl] = useState(null);
   const [file, setFile] = useState("");
+  const [progress, setProgress] = useState(0);
+
   const handleFileInput = (evnt) => {
     setFile(evnt.target.files[0]);
-    setPreviewAvatar(URL.createObjectURL(evnt.target.files[0]));
-    console.log("haha");
+    setPreviewImgUrl(URL.createObjectURL(evnt.target.files[0]));
+    // console.log(previewImgUrl);
+    //Setup Logic:
+    // setIsLoading(true);
+    // Tesseract.recognize(previewImgUrl, "eng", {
+    //   logger: (m) => console.log(previewImgUrl),
+    // }).then(({ data: { text } }) => {
+    //   console.log(text);
+    // });
+  };
+  const handleConvert = (evnt) => {
+    console.log(previewImgUrl);
+
+    setIsLoading(true);
+    Tesseract.recognize(previewImgUrl, "eng", {
+      logger: (m) => console.log(previewImgUrl),
+    }).then(({ data: { text } }) => {
+      console.log(text);
+    });
   };
   return (
     <div>
-      <Typography
-        variant="h5"
-        sx={{
-          fontSize: "1.5rem",
-          fontWeight: 500,
-          textAlign: "center",
-          marginBottom: "1rem",
-          color: "black",
-        }}
-      >
-        Image To Text
-      </Typography>
-      {/* {!isLoading && !text & <input></input>} */}
-      <TextField
-        // onChange={handleInputChange}
-        required
-        hiddenLabel
-        // fullWidth
-        value=""
-        variant="filled"
-        size="small"
-        type="text"
-        name="username"
-      />
       <Grid
         md={4}
         item
@@ -46,30 +44,93 @@ function SearchBar() {
         justifyContent={"center"}
         marginX={{ xs: "auto" }}
       >
-        <Box display="flex" flexDirection={"column"} alignItems={"center"}>
-          <Avatar
-            src={previewAvatar}
+        {!isLoading && (
+          <Typography
+            variant="h5"
             sx={{
-              width: 128,
-              height: 128,
-              border: "solid 1px var(--color3)",
-              marginBottom: 2,
+              fontSize: "1.5rem",
+              fontWeight: 500,
+              textAlign: "center",
+              marginBottom: "1rem",
+              color: "black",
             }}
-          />
-          <Button
-            sx={{ fontWeight: "bold", textTransform: "none" }}
-            component="label"
-            defaultValue={previewAvatar}
           >
-            Upload
-            <input
-              hidden
-              accept=".png, .jpeg, .jpg*"
-              type="file"
-              onChange={handleFileInput}
+            Image To Text
+          </Typography>
+        )}
+        {!isLoading && !textConvert && (
+          <Box display="flex" flexDirection={"column"} alignItems={"center"}>
+            <TextField
+              // onChange={handleInputChange}
+              required
+              hiddenLabel
+              // fullWidth
+              value=""
+              variant="filled"
+              size="small"
+              type="text"
+              name="username"
             />
-          </Button>
-        </Box>
+            <Avatar
+              src={previewImgUrl}
+              sx={{
+                width: 128,
+                height: 128,
+                border: "solid 1px var(--color3)",
+                marginBottom: 2,
+              }}
+            />
+            <Button
+              sx={{ fontWeight: "bold", textTransform: "none" }}
+              component="label"
+              defaultValue={previewImgUrl}
+              // onClick={handleConvert}
+            >
+              Select Image
+              <input
+                hidden
+                accept=".png, .jpeg, .jpg*"
+                type="file"
+                onChange={handleFileInput}
+              />
+            </Button>
+            <Button
+              sx={{ fontWeight: "bold", textTransform: "none" }}
+              component="label"
+              defaultValue={previewImgUrl}
+              onClick={handleConvert}
+            >
+              Convert
+            </Button>
+          </Box>
+        )}
+        {isLoading && (
+          <Box>
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: "1.5rem",
+                fontWeight: 500,
+                textAlign: "center",
+                marginBottom: "1rem",
+                color: "black",
+              }}
+            >
+              is Loading {progress}
+            </Typography>
+          </Box>
+        )}
+        {!isLoading && textConvert && (
+          <Box>
+            <TextareaAutosize
+              maxRows={6}
+              minRows={6}
+              aria-label="minimum height"
+              // value={text}
+              style={{ width: 200 }}
+            />
+          </Box>
+        )}
       </Grid>
     </div>
   );
