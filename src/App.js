efc5b5d64d1e96e6d6968f2a0356cc3e6e4fb999
tanmui1992 <tanmui1992@gmail.com>
2022-12-11 10:41:26 +0800

    feat: update app ape

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import SideBar from "./components/SideBar/SideBar";
 import MainBox from "./components/MainBox/MainBox";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/homepage";
 function App() {
   return (
     <div className="App">
